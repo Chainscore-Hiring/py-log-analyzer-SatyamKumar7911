51e1043359d941e5ae2868fc55e745633d59e471
Satyam Kumar <147496093+SatyamKumar7911@users.noreply.github.com>
2024-12-02 22:28:11 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,10 @@
+# test_vectors/sample_logs.txt
+"""
+2024-01-24 10:15:32.123 INFO Request processed in 127ms
+2024-01-24 10:15:33.001 ERROR Database connection failed
+2024-01-24 10:15:34.042 INFO Request processed in 95ms
+2024-01-24 10:15:35.056 INFO Request processed in 150ms
+2024-01-24 10:15:36.072 ERROR Timeout while processing request
+2024-01-24 10:15:37.089 INFO Request processed in 85ms
+2024-01-24 10:15:38.104 INFO Request processed in 110ms
+"""
